15aef263b6ff66ba4caa9c8eeb25eaa6f4a52f76
PiyushJi040 <piyushji0400@gmail.com>
2026-02-09 17:46:10 +0530

    Fix: Use HashRouter for GitHub Pages compatibility

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import { ThemeProvider, ThemeToggle } from './components/ThemeProvider';
 import { NotificationProvider } from './components/NotificationSystem';
 import GlobalBackground from './components/GlobalBackground';
 import Landing from './components/Landing';
-import FactCheck from './components/FactCheck';
 import AdvancedFactCheck from './components/AdvancedFactCheck';
 import Result from './components/Result';
 import './App.css';
 
 function App() {
   return (
-    <ThemeProvider>
-      <NotificationProvider>
-        <div className="App">
-          <GlobalBackground />
-          <ThemeToggle />
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/fact-check" element={<AdvancedFactCheck />} />
-            <Route path="/result" element={<Result />} />
-          </Routes>
-        </div>
-      </NotificationProvider>
-    </ThemeProvider>
+    <HashRouter>
+      <ThemeProvider>
+        <NotificationProvider>
+          <div className="App">
+            <GlobalBackground />
+            <ThemeToggle />
+            <Routes>
+              <Route path="/" element={<Landing />} />
+              <Route path="/fact-check" element={<AdvancedFactCheck />} />
+              <Route path="/result" element={<Result />} />
+            </Routes>
+          </div>
+        </NotificationProvider>
+      </ThemeProvider>
+    </HashRouter>
   );
 }
 
